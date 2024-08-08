18e20bfd13251aa6ed277eb721b3344c3086c802
Frannko <Frannko13579@gmail.com>
2024-08-08 03:02:21 -0300

    Added Hamburger Menu Animation

--- Honda/components/js/main.js
+++ Honda/components/js/main.js
@@ -1,3 +1,22 @@
 document.getElementById('navbarSideCollapse').addEventListener('click', function () {
     document.getElementById('navbarNav').classList.toggle('open');
 });
+
+document.getElementById('openText').addEventListener('click', function () {
+    document.getElementById('navbarNav').classList.toggle('open');
+});
+
+function toggleMenu() {
+    var menu = document.getElementById('navbarSideCollapse');
+    menu.classList.toggle('active');
+
+    if (menu.classList.contains('active')) {
+        // Esperar a que la primera animación termine antes de comenzar la segunda
+        setTimeout(function() {
+            menu.classList.add('second-animation');
+        }, 300); // Tiempo en milisegundos que corresponde a la duración de la primera animación
+    } else {
+        // Eliminar la clase de la segunda animación inmediatamente si se desactiva
+        menu.classList.remove('second-animation');
+    }
+}
